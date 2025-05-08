92cd2c3a157e18df1be4d649f9df894f972d6452
DianaNtz <110336168+DianaNtz@users.noreply.github.com>
2025-05-08 19:51:05 +0200

    added code description

---
+++ Raytracing.py
@@ -0,0 +1,7 @@
+"""
+The code below was written by @author: https://github.com/DianaNtz and is an
+implementation of the 2nd order Runge Kutta time integration method for the
+geodesic equation of a Schwarzschild black hole.
+For more details on the code requirements and licences see
+https://github.com/DianaNtz/Raytracing.
+"""
